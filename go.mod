458cad6041b5bb33850adacfefccf75b79eeaa1e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2020-10-11 23:58:06 +0700

    app say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ProgrammerZamanNow/app-say-hello
+
+go 1.15
+
+require github.com/ProgrammerZamanNow/go-say-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/ProgrammerZamanNow/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Eko"))
+}
